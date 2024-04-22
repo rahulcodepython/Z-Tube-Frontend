c82ac49cfd9fe55788622258d8f0660ea4217bc7
Rahul Das <77882624+rahulcodepython@users.noreply.github.com>
2024-04-22 19:05:08 +0530

    Basic pagination system is done which is not working.

--- src/app/(index)/(main)/user/[username]/@feed/page.jsx
+++ src/app/(index)/(main)/user/[username]/@feed/page.jsx
@@ -21,10 +21,18 @@ const Feed = ({ params }) => {
     const { data, setData } = React.useContext(DataContext)
 
     const [loading, setLoading] = React.useState('feedPost' in data ? decodeURIComponent(params.username) in data.feedPost ? false : true : true)
+    const [pagination, setPagination] = React.useState({
+        count: 0,
+        next: null,
+        previous: null,
+    })
+    const [page, setPage] = React.useState(1)
+
+    const pageNumbers = Array.from({ length: calculatePages(pagination.count, 3) }, (_, index) => index + 1);
 
     React.useEffect(() => {
         const handler = async () => {
-            await FetchFeedPost(accessToken, params.username, setData, setLoading);
+            await FetchFeedPost(accessToken, params.username, setData, setLoading, setPagination, page);
         }
         handler();
     }, [])
@@ -40,35 +48,30 @@ const Feed = ({ params }) => {
         <Pagination>
             <PaginationContent>
                 <PaginationItem>
-                    <PaginationPrevious href="#" disabled />
+                    {pagination.previous && <PaginationPrevious href="#" onClick={() => setPage(page - 1)} />}
                 </PaginationItem>
+                {
+                    pageNumbers.map((item, index) => {
+                        return <PaginationItem key={index}>
+                            <PaginationLink href="#" isActive={item === page ? true : false} onClick={() => setPage(item)}>
+                                {item}
+                            </PaginationLink>
+                        </PaginationItem>
+                    })
+                }
                 <PaginationItem>
-                    <PaginationLink href="#">1</PaginationLink>
-                </PaginationItem>
-                <PaginationItem>
-                    <PaginationLink href="#" isActive>
-                        2
-                    </PaginationLink>
-                </PaginationItem>
-                <PaginationItem>
-                    <PaginationLink href="#">3</PaginationLink>
-                </PaginationItem>
-                <PaginationItem>
-                    <PaginationEllipsis />
-                </PaginationItem>
-                <PaginationItem>
-                    <PaginationNext href="#" />
+                    {pagination.next && <PaginationNext href="#" onClick={() => setPage(page + 1)} />}
                 </PaginationItem>
             </PaginationContent>
         </Pagination>
     </div>
 }
 
-const FetchFeedPost = async (accessToken, username, setData, setLoading) => {
+const FetchFeedPost = async (accessToken, username, setData, setLoading, setPagination, page) => {
     const options = {
         headers: {
             Authorization: `JWT ${accessToken}`
-        }, url: `${process.env.BASE_API_URL}/feed/posts/${username}/`, method: 'GET',
+        }, url: `${process.env.BASE_API_URL}/feed/posts/${username}/${page <= 1 ? '' : `?page=${page}`}`, method: 'GET',
     };
 
     await axios.request(options).then(response => {
@@ -78,12 +81,32 @@ const FetchFeedPost = async (accessToken, username, setData, setLoading) => {
             const resultFeedPost = 'feedPost' in newData ? decodeURIComponent(username) in newData.feedPost ? [...newData.feedPost[decodeURIComponent(username)], ...response.data.results] : response.data.results : response.data.results
 
             newData.feedPost = {
-                [decodeURIComponent(username)]: []
-                // [decodeURIComponent(username)]: response.status === 200 ? resultFeedPost : []
+                [decodeURIComponent(username)]: response.status === 200 ? resultFeedPost : []
             }
             return newData;
         })
+        setPagination({
+            count: response.data.count,
+            next: response.data.next,
+            previous: response.data.previous,
+        })
     }).finally(() => setLoading(false))
+}
+
+const calculatePages = (totalItems, itemsPerPage) => {
+    // Calculate the number of full pages needed
+    const fullPages = Math.floor(totalItems / itemsPerPage);
+
+    // If there are any remaining items that don't make a full page
+    const remainder = totalItems % itemsPerPage;
+
+    // If remainder is greater than 0, we need an additional page for them
+    const additionalPage = remainder > 0 ? 1 : 0;
+
+    // Total pages required
+    const totalPages = fullPages + additionalPage;
+
+    return totalPages;
 }
 
 export default Feed
