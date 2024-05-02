546c92be34dc03ed26956963cbe1628993cb2556
Rahul Das <77882624+rahulcodepython@users.noreply.github.com>
2024-05-02 16:09:23 +0530

    Firebase is configured & Upload file function is done.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,6 @@
 import type {AccessToken} from "@/context/AuthContext";
+import { ref, uploadBytes, getDownloadURL, StorageReference } from 'firebase/storage'
+import {analytics} from "@/utils/firebase-config";
 
 export const Decrypt = (token: AccessToken, key: String|undefined) => {
     let decryptedToken = '';
@@ -19,3 +21,13 @@ export const Encrypt = (token: AccessToken, key: String|undefined) => {
     }
     return encryptedToken;
 }
+
+interface FileObject extends File {}
+
+export const UploadMediaFiles = async (item: FileObject, uploadFilePath: string): Promise<string> => {
+    let url: string;
+    const fileref: StorageReference = ref(analytics, uploadFilePath);
+    const response = await uploadBytes(fileref, item);
+    url = await getDownloadURL(response.ref);
+    return url;
+};
