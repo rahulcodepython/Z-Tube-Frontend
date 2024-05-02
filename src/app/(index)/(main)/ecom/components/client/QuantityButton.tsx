8a886a4d7dbcce792052e1584fb1767d7826708f
Rahul Das <77882624+rahulcodepython@users.noreply.github.com>
2024-05-03 04:25:12 +0530

    Single Product page is added.
    The single product page along with it's component.

---
+++ src/app/(index)/(main)/ecom/components/client/QuantityButton.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {Button} from "@/components/ui/button";
+
+const QuantityButton = ({ maxAmount }: {maxAmount: number}) => {
+    const [quantity, setQuantity] = React.useState<number>(1)
+
+    return <div className="flex items-center justify-start">
+        <div className="flex items-center gap-4">
+            <Button variant="outline" className="px-6 py-0" onClick={() => setQuantity(pre => pre - 1 <= 0 ? pre : pre - 1)}> - </Button>
+            <span> {quantity} </span>
+            <Button variant="outline" className="px-6 py-0" onClick={() => setQuantity(pre => pre + 1 > maxAmount ? pre : pre + 1)}> + </Button>
+        </div>
+    </div>
+}
+
+export default QuantityButton;
